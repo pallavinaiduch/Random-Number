dd7fae72c54b034a4f7a609a9bd30616a8d33bfc
PallaviNaiduCh <pallavichappa04@gmail.com>
2023-12-16 01:00:50 +0530

    Final Commit

--- app/src/main/java/com/example/randomnumber/MainActivity.java
+++ app/src/main/java/com/example/randomnumber/MainActivity.java
@@ -23,12 +23,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 EditText f=(EditText) findViewById(R.id.fetch);
                 TextView result=(TextView) findViewById(R.id.put);
+                TextView result1=(TextView) findViewById(R.id.Second);
+                TextView Add=(TextView) findViewById(R.id.Addition);
+                TextView Subs=(TextView) findViewById(R.id.Subb);
+                TextView Mul=(TextView) findViewById(R.id.multip);
 
 
                 int range =Integer.parseInt(f.getText().toString());
                 Random r=new Random();
                 int rand_no=r.nextInt(range - 0)+ 0;
                 result.setText(rand_no+" ");
+                int range1 =Integer.parseInt(f.getText().toString());
+                Random r1=new Random();
+                int rand_no1=r1.nextInt(range - 0)+ 0;
+                result1.setText(rand_no1+" ");
+                String Ad=String.valueOf(rand_no + rand_no1);
+                ((TextView)Add).setText(String.valueOf(Ad));
+                String su=String.valueOf(rand_no - rand_no1);
+                ((TextView)Subs).setText(String.valueOf(su));
+                String Muli=String.valueOf(rand_no * rand_no1);
+                ((TextView)Mul).setText(String.valueOf(Muli));
+
+
 
             }
         });
